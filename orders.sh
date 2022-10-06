a4b41ca92ab8a70e9585cca4c9952e304baedf27
AnuTechnologies <anutechologies27@gmail.com>
2022-10-06 07:16:08 +0530

    added orders & payments

---
+++ orders.sh
@@ -0,0 +1,4 @@
+Customer Name              Order Names
+Reddy			     122278221
+God Father		     719902828
+Kishor		             889197711
---
+++ payments.sh
@@ -0,0 +1,6 @@
+Bank Name                Amount
+
+SBI                      7722
+BOB                      162
+ICICI                    11881
+KOTAK                    1771
